b094fc518cd3382c20992a55e0805f0bfa2c4e62
Graham Thompson <captainhammy@gmail.com>
2012-04-22 21:43:23 -0700

    Adding PointsFromVoxesls.

---
+++ SOP_PointsFromVoxels.C
@@ -0,0 +1,149 @@
+/*
+ * Produced by:
+ *      Graham Thompson
+ *      captainhammy@gmail.com
+ *      www.captainhammy.com
+ *
+ * Description:
+ *     	Create a point at the center of each voxel.
+ *
+ * Name: SOP_PointsFromVoxels.C
+ * 
+ * Version: 1.0
+*/
+
+#include "SOP_PointsFromVoxels.h"
+
+#include <GEO/GEO_PrimVolume.h>
+#include <PRM/PRM_Include.h>
+#include <UT/UT_VoxelArray.h>
+
+#include <OP/OP_Operator.h>
+#include <OP/OP_OperatorTable.h>
+#include <UT/UT_DSOVersion.h>
+
+void
+newSopOperator(OP_OperatorTable *table)
+{ 
+    table->addOperator(
+        new OP_Operator("pointsfromvoxels",
+                        "Points From Voxels",
+                        SOP_PointsFromVoxels::myConstructor,
+                        SOP_PointsFromVoxels::myTemplateList,
+                        1,
+                        1,
+                        0)
+    );
+}
+
+OP_Node *
+SOP_PointsFromVoxels::myConstructor(OP_Network *net, 
+                                    const char *name,
+                                    OP_Operator *op)
+{
+    return new SOP_PointsFromVoxels(net, name, op);
+}
+
+SOP_PointsFromVoxels::SOP_PointsFromVoxels(OP_Network *net, 
+                                           const char *name, 
+                                           OP_Operator *op): 
+    SOP_Node(net, name, op) {}
+
+static PRM_Name names[] =
+{
+    PRM_Name("prim", "Primitive Number"),
+    PRM_Name("cull", "Cull Empty"),
+    PRM_Name(0)
+};
+
+static PRM_Default defaults[] =
+{
+    PRM_Default(0),
+    PRM_Default(0)
+};
+
+static PRM_Range        prim_range(PRM_RANGE_RESTRICTED, 0, PRM_RANGE_UI, 10);
+
+PRM_Template
+SOP_PointsFromVoxels::myTemplateList[] = {
+    PRM_Template(PRM_INT, 1, &names[0], &defaults[0], 0, &prim_range),
+    PRM_Template(PRM_TOGGLE, 1, &names[1], &defaults[1]),
+    PRM_Template()
+};
+
+OP_ERROR
+SOP_PointsFromVoxels::cookMySop(OP_Context &context)
+{
+    fpreal 			now;
+    unsigned			primnum;
+    bool                        cull;
+
+    GA_Offset                   ptOff;
+
+    UT_Vector3			pos;
+
+    const GU_Detail		*input_geo;
+    const GEO_Primitive		*prim;
+    const GEO_PrimVolume	*vol;
+
+    UT_VoxelArrayIteratorF	vit;
+
+    now = context.myTime;
+
+    if (lockInputs(context) >= UT_ERROR_ABORT)
+        return error();
+
+    // Get the primitive number.
+    primnum = PRIM(now);
+    // Check for culling.
+    cull = CULL(now);
+
+    // Clear out the detail since we only want our new points.
+    gdp->clearAndDestroy();
+    // Get the input geometry as read only.
+    GU_DetailHandleAutoReadLock gdl(inputGeoHandle(0));
+    input_geo = gdl.getGdp();
+
+    // Primitive number is valid.
+    if (primnum < input_geo->getNumPrimitives())
+    {
+	// Get the primitive we need.
+	prim = input_geo->primitives()(primnum);
+	// The primitive is a volume primitive.
+	if (prim->getTypeId().get() == GEO_PRIMVOLUME)
+	{
+	    // Get the actual PrimVolume.
+	    vol = (const GEO_PrimVolume *)prim;
+
+            // Get a voxel read handle from the primitive.
+	    UT_VoxelArrayReadHandleF	vox(vol->getVoxelHandle());
+            // Attach the voxel iterator to the handle.
+	    vit.setHandle(vox);
+
+            // Iterate over all the voxels.
+	    for (vit.rewind(); !vit.atEnd(); vit.advance())
+	    {
+                // If we are culling empty voxels and this voxel's
+                // value is 0 we skip it.
+                if (cull && vit.getValue() == 0)
+                    continue;
+                // Convert the voxel index to a position.
+                vol->indexToPos(vit.x(), vit.y(), vit.z(), pos);
+                // Create a point and set it to the position of the
+                // voxel.
+                ptOff = gdp->appendPointOffset();
+                gdp->setPos3(ptOff, pos);
+	    }
+	}
+	// Primitive isn't a volume primitive.
+	else
+	    addError(SOP_MESSAGE, "Not a volume primitive.");
+    }
+    // Picked a primitive number that is out of range.
+    else
+	addWarning(SOP_MESSAGE, "Invalid source index. Index out of range.");
+
+    unlockInputs();
+    return error();
+}
+
---
+++ SOP_PointsFromVoxels.h
@@ -0,0 +1,41 @@
+/*
+ * Produced by:
+ *      Graham Thompson
+ *      captainhammy@gmail.com
+ *      www.captainhammy.com
+ *
+ * Description:
+ *     	Create a point at the center of each voxel.
+ * 
+ * Name: SOP_PointsFromVoxels.h
+ *
+ * Version: 1.0
+*/
+
+#ifndef __SOP_PointsFromVoxels_h__
+#define __SOP_PointsFromVoxels_h__
+
+#include <SOP/SOP_Node.h>
+
+class SOP_PointsFromVoxels: public SOP_Node
+{
+public:
+    static OP_Node      *myConstructor(OP_Network *,
+				       const char *, 
+				       OP_Operator *);
+    static PRM_Template myTemplateList[];
+
+protected:
+			SOP_PointsFromVoxels(OP_Network *,
+					     const char *,
+					     OP_Operator *);
+    virtual		~SOP_PointsFromVoxels() {};
+    virtual OP_ERROR	cookMySop(OP_Context &context);
+
+private:
+    unsigned		PRIM(fpreal t) { return evalInt("prim", 0, t); }
+    unsigned		CULL(fpreal t) { return evalInt("cull", 0, t); }
+
+};
+
+#endif
