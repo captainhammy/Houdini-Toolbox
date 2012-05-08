bcf738fdf8af59e84a2ff0ab7dbed07022ee15be
Graham Thompson <captainhammy@gmail.com>
2012-05-08 07:19:33 -0700

    Additional attempts at threading and group support.

--- SOP_IdAttribCopy.C
+++ SOP_IdAttribCopy.C
@@ -24,6 +24,55 @@
 #include <UT/UT_DSOVersion.h>
 #include <UT/UT_WorkArgs.h>
 
+void
+SOP_IdAttribCopy::testBuildPartial(const GA_SplittableRange &range,
+                                   GA_AttributeRefMap hmap,
+                                   const GA_Attribute *id_gah,
+                                   const IdOffsetMap &id_map,
+                                   const UT_JobInfo &info)
+{
+    exint                   id;
+
+    GA_Offset               start, end;
+    GA_ROPageHandleI        id_ph(id_gah);
+
+    IdOffsetMap::const_iterator map_it;
+
+    hmap.initThreadInstance();
+
+    // Iterate over the pages.
+    for (GA_PageIterator pit = range.beginPages(); !pit.atEnd(); ++pit)
+    {
+        // Set the handle to this page.
+        id_ph.setPage(*pit);
+
+        // Get the offsets in the page.
+        for (GA_Iterator it(pit.begin()); it.blockAdvance(start, end); )
+        {
+            // Iterate over the offsets in the page.
+            for (GA_Offset pt = start; pt < end; ++pt)
+            {
+                // Get the id value for this point.
+                id = id_ph.get(pt);
+                // Try to find the corresponding id.
+                map_it = id_map.find(id);
+                // If the iterator isn't at the end, copy the value.
+                if (map_it != id_map.end())
+                {
+                    // Copy the point attributes from the offset pointed
+                    // to by the found 'id' value to the current point.
+                    hmap.copyValue(GA_ATTRIB_POINT,
+                                           pt,
+                                           GA_ATTRIB_POINT,
+                                           (*map_it).second);
+                }
+            }
+        }
+    }
+
+}
+
+
 // This class is used to copy point attributes in a threaded manner.
 class AttributeIdCopier {
 public:
@@ -80,18 +129,6 @@ private:
 
 };
 
-// The function to run the threaded attribute copying.
-void
-copyById(const GA_Range &range,
-         GA_AttributeRefMap *hmap,
-         const GA_Attribute *id,
-         IdOffsetMap *id_map)
-{
-    // Engage attribute copying across threads.
-    UTparallelFor(GA_SplittableRange(range),
-                  AttributeIdCopier(hmap, id, id_map));
-}
-
 void 
 newSopOperator(OP_OperatorTable *table)
 {
@@ -117,16 +154,18 @@ SOP_IdAttribCopy::myConstructor(OP_Network *net,
 SOP_IdAttribCopy::SOP_IdAttribCopy(OP_Network *net,
                                    const char *name, 
                                    OP_Operator *op):
-    SOP_Node(net, name, op) {}
+    SOP_Node(net, name, op), myGroup(0) {}
 
 static PRM_Name names[] =
 {
+    PRM_Name("group", "Group"),
     PRM_Name("attributes", "Attributes to copy"),
     PRM_Name("copyp", "Accept \"P\""),
 };
 
 static PRM_Default defaults[] =
 {
+    PRM_Default(0, ""),
     PRM_Default(0, "*"),
     PRM_Default(0),
 };
@@ -136,8 +175,9 @@ static PRM_ChoiceList attribMenu((PRM_ChoiceListType)(PRM_CHOICELIST_TOGGLE),
 
 PRM_Template
 SOP_IdAttribCopy::myTemplateList[] = {
-    PRM_Template(PRM_STRING, 1, &names[0], &defaults[0], &attribMenu),
-    PRM_Template(PRM_TOGGLE, 1, &names[1], &defaults[1]),
+    PRM_Template(PRM_STRING, 1, &names[0], &defaults[0], &SOP_Node::pointGroupMenu),
+    PRM_Template(PRM_STRING, 1, &names[1], &defaults[1], &attribMenu),
+    PRM_Template(PRM_TOGGLE, 1, &names[2], &defaults[2]),
     PRM_Template()
 };
 
@@ -170,6 +210,67 @@ SOP_IdAttribCopy::buildMenu(void *data,
                            &SOP_IdAttribCopy::validateAttrib);
 }
 
+
+OP_ERROR
+SOP_IdAttribCopy::cookInputGroups(OP_Context &context, int alone)
+{
+    // If we are called by the handle, then "alone" equals 1.  In that
+    // case, we have to lock the inputs oursevles, and unlock them
+    // before exiting this method.
+    if (alone)
+        if (lockInputs(context) >= UT_ERROR_ABORT)
+            return error();
+
+    UT_String    grp_name;
+
+    // The "gdp" variable is only available if we are called from the SOP
+    // itself.  So, if we are called by a handle, we have to get the
+    // geometry oursevles.
+    GU_Detail   *pgdp = alone ? (GU_Detail *)inputGeo(0, context) : gdp;
+
+    myGroup = 0;
+
+    GROUP(grp_name, context.getTime());                // Get the group string.
+
+    // If the group string is not null, then we try to parse the group.
+    if (grp_name.isstring())
+    {
+        myGroup = parsePointGroups((const char *)grp_name, pgdp);
+
+        // If the group is not valid, then the group string is invalid
+        // as well.  Thus, we add an error to this SOP.
+        if (!myGroup)
+        {
+            addError(SOP_ERR_BADGROUP, grp_name);
+        }
+        else if (!alone)
+        {
+            // If the parsed group is valid, then we want to highlight
+            // only the group.  The second argument of "1" means that
+            // we want the selection to have the same type as our group.
+            select(*const_cast<GA_PointGroup*>(myGroup), 1);
+        }
+    }
+    else if (!alone)
+    {
+        // If no group string is specified, then we operate on the entire
+        // geometry, so we highlight every point for this SOP.
+        select(GU_SPoint);
+    }
+
+    // This is where we notify our handles (if any) if the inputs have changed.
+    checkInputChanged(0, -1, myDetailGroupPair, pgdp, myGroup);
+
+    // If we are called by the handles, then we have to unlock our inputs.
+    if (alone)
+    {
+        destroyAdhocGroups();
+        unlockInputs();
+    }
+
+    return error();
+}
+
 OP_ERROR
 SOP_IdAttribCopy::cookMySop(OP_Context &context)
 {
@@ -198,113 +299,124 @@ SOP_IdAttribCopy::cookMySop(OP_Context &context)
     // Duplicate the incoming geometry.
     duplicateSource(0, context);
 
-    // Get the 2nd input geometry as read only.
-    GU_DetailHandleAutoReadLock source_gdl(inputGeoHandle(1));
-    src_geo = source_gdl.getGdp();
-
-    // Construct an attribute reference map to map attributes.
-    GA_AttributeRefMap hmap(*gdp, src_geo);
-
-    // Get the attribute selection string.
-    ATTRIBUTES(pattern, now);
-    
-    // Try to find the 'id' point attribute on the 1st input geometry.
-    id_gah = gdp->findPointAttribute(GA_SCOPE_PUBLIC, "id");
-    // If it doesn't exist, display a node error message and exit.
-    if (id_gah.isInvalid())
+    if (error() < UT_ERROR_ABORT && cookInputGroups(context) < UT_ERROR_ABORT)
     {
-        addError(SOP_MESSAGE, "Input 1 has no 'id' attribute.");
-        unlockInputs();
-        return error();
-    }
+        // Get the 2nd input geometry as read only.
+        GU_DetailHandleAutoReadLock source_gdl(inputGeoHandle(1));
+        src_geo = source_gdl.getGdp();
 
-    // Try to find the 'id' point attribute on the 2nd input geometry.
-    srcid_gah = src_geo->findPointAttribute(GA_SCOPE_PUBLIC, "id");
-    // If it doesn't exist, display a node error message and exit.
-    if (srcid_gah.isInvalid())
-    {
-        addError(SOP_MESSAGE, "Input 2 has no 'id' attribute.");
-        unlockInputs();
-        return error();
-    }
-    // Bind the page handle to the attribute.
-    srcid_ph.bind(srcid_gah.getAttribute());
+        // Construct an attribute reference map to map attributes.
+        GA_AttributeRefMap hmap(*gdp, src_geo);
 
-    // Make sure we entered something.
-    if (pattern.length() > 0)
-    {
-        // Tokenize the pattern.
-        pattern.tokenize(tokens, " ");
-
-        // Get the point attributes on the 2nd input geometry.
-        dict = &src_geo->pointAttribs();
-
-        // Iterate over all the point attributes.
-        for (GA_AttributeDict::iterator it=dict->begin(GA_SCOPE_PUBLIC);
-             !it.atEnd();
-             ++it)
+        // Get the attribute selection string.
+        ATTRIBUTES(pattern, now);
+        
+        // Try to find the 'id' point attribute on the 1st input geometry.
+        id_gah = gdp->findPointAttribute(GA_SCOPE_PUBLIC, "id");
+        // If it doesn't exist, display a node error message and exit.
+        if (id_gah.isInvalid())
         {
-            // The current attribute.
-            source_attr = it.attrib();
-            // Get the attribute name.
-            name = source_attr->getName();
+            addError(SOP_MESSAGE, "Input 1 has no 'id' attribute.");
+            unlockInputs();
+            return error();
+        }
 
-            // Skip the 'id' attribute.
-            if (name == "id")
-                continue;
+        // Try to find the 'id' point attribute on the 2nd input geometry.
+        srcid_gah = src_geo->findPointAttribute(GA_SCOPE_PUBLIC, "id");
+        // If it doesn't exist, display a node error message and exit.
+        if (srcid_gah.isInvalid())
+        {
+            addError(SOP_MESSAGE, "Input 2 has no 'id' attribute.");
+            unlockInputs();
+            return error();
+        }
+        // Bind the page handle to the attribute.
+        srcid_ph.bind(srcid_gah.getAttribute());
 
-            // If the name doesn't match our pattern, skip it.
-            if (!name.matchPattern(tokens))
-                continue;
+        // Make sure we entered something.
+        if (pattern.length() > 0)
+        {
+            // Tokenize the pattern.
+            pattern.tokenize(tokens, " ");
 
-            // Try to find the attribute on the first input geometry.
-            attr_gah = gdp->findPointAttrib(*source_attr);
-            // If it doesn't exist, create a new attribute.
-            if (attr_gah.isInvalid())
+            // Get the point attributes on the 2nd input geometry.
+            dict = &src_geo->pointAttribs();
+
+            // Iterate over all the point attributes.
+            for (GA_AttributeDict::iterator it=dict->begin(GA_SCOPE_PUBLIC);
+                 !it.atEnd();
+                 ++it)
             {
-                // Create a new point attribute on the current geometry
-                // that is the same as the source attribute.  Append it and
-                // the source to the map.
-                hmap.append(gdp->addPointAttrib(source_attr).getAttribute(),
-                            source_attr);
+                // The current attribute.
+                source_attr = it.attrib();
+                // Get the attribute name.
+                name = source_attr->getName();
+
+                // Skip the 'id' attribute.
+                if (name == "id")
+                    continue;
+
+                // If the name doesn't match our pattern, skip it.
+                if (!name.matchPattern(tokens))
+                    continue;
+
+                // Try to find the attribute on the first input geometry.
+                attr_gah = gdp->findPointAttrib(*source_attr);
+                // If it doesn't exist, create a new attribute.
+                if (attr_gah.isInvalid())
+                {
+                    // Create a new point attribute on the current geometry
+                    // that is the same as the source attribute.  Append it and
+                    // the source to the map.
+                    hmap.append(gdp->addPointAttrib(source_attr).getAttribute(),
+                                source_attr);
+                }
+            }
+
+            // If we are allowing 'P' to be copied, see if we should copy it.
+            if (COPYP(now))
+            {
+                name = "P";
+                // If 'P' matches our pattern, add it to the map.
+                if (name.matchPattern(tokens))
+                    hmap.append(gdp->getP(), src_geo->getP());
+            }
+        }
+        // Selected nothing so don't do anything.
+        else
+        {
+            unlockInputs();
+            return error();
+        }
+
+        // Iterate over the 2nd input geometry points.
+        for (GA_Iterator it(src_geo->getPointRange()); it.blockAdvance(start, end); )
+        {
+            // Set the page handle to the start of this page.
+            srcid_ph.setPage(start);
+            
+            // Iterate over all the points in the page.
+            for (GA_Offset pt = start; pt < end; ++pt)
+            {
+                // Get the 'id' value for the point.
+                id = srcid_ph.get(pt);
+                // Store a mapping from the 'id' to the point.
+                id_map[id] = pt;
             }
         }
 
-        // If we are allowing 'P' to be copied, see if we should copy it.
-        if (COPYP(now))
-        {
-            name = "P";
-            // If 'P' matches our pattern, add it to the map.
-            if (name.matchPattern(tokens))
-                hmap.append(gdp->getP(), src_geo->getP());
-        }
-    }
-    // Selected nothing so don't do anything.
-    else
-    {
-        unlockInputs();
-        return error();
-    }
+/*
+        // Engage attribute copying across threads.
+        UTparallelFor(GA_SplittableRange(gdp->getPointRange(myGroup)),
+                      AttributeIdCopier(&hmap, id_gah.getAttribute(), &id_map));
+*/
 
-    // Iterate over the 2nd input geometry points.
-    for (GA_Iterator it(src_geo->getPointRange()); it.blockAdvance(start, end); )
-    {
-        // Set the page handle to the start of this page.
-        srcid_ph.setPage(start);
-        
-        // Iterate over all the points in the page.
-        for (GA_Offset pt = start; pt < end; ++pt)
-        {
-            // Get the 'id' value for the point.
-            id = srcid_ph.get(pt);
-            // Store a mapping from the 'id' to the point.
-            id_map[id] = pt;
-        }
+
+        const GA_Attribute *attrib = id_gah.getAttribute();
+        const GA_SplittableRange range = GA_SplittableRange(gdp->getPointRange(myGroup));
+        testBuild(range, hmap, attrib, id_map);
+
     }
-
-    // Copy the attributes.
-    copyById(gdp->getPointRange(), &hmap, id_gah.getAttribute(), &id_map);
-
     unlockInputs();
     return error();
 }
--- SOP_IdAttribCopy.h
+++ SOP_IdAttribCopy.h
@@ -17,7 +17,14 @@
 #define __SOP_IdAttribCopy_h__
 
 #include <boost/unordered_map.hpp>
+#include <GA/GA_AttributeRefMap.h>
+#include <GA/GA_AttributeRefMap.h>
+#include <GA/GA_SplittableRange.h>
 #include <SOP/SOP_Node.h>
+
+// Simple mapping to a boost::unordered_map that is meant to be a mapping
+// between point id values and their corresponding point offset.
+typedef boost::unordered_map<exint, GA_Offset> IdOffsetMap;
 
 class SOP_IdAttribCopy: public SOP_Node
 {
@@ -33,6 +40,12 @@ public:
 				  const PRM_Parm *);
     static bool	     	validateAttrib(const GA_Attribute *,
 				       void *);
+
+    /// This method is created so that it can be called by handles.  It only
+    /// cooks the input group of this SOP.  The geometry in this group is
+    /// the only geometry manipulated by this SOP.
+    virtual OP_ERROR             cookInputGroups(OP_Context &context, 
+                                                int alone = 0);
 protected:
 			SOP_IdAttribCopy(OP_Network *, 
 					 const char *, 
@@ -40,14 +53,34 @@ protected:
     virtual		~SOP_IdAttribCopy() {};
     virtual OP_ERROR	cookMySop(OP_Context &);
 
+
+    THREADED_METHOD4(SOP_IdAttribCopy, range.canMultiThread(), testBuild,
+		     const GA_SplittableRange &, range,
+		     GA_AttributeRefMap, hmap,
+		     const GA_Attribute *, id_gah,
+		     const IdOffsetMap &, id_map);
+
+    void testBuildPartial(const GA_SplittableRange &range,
+			  GA_AttributeRefMap hmap,
+			  const GA_Attribute *id_gah,
+			  const IdOffsetMap &id_map,
+			  const UT_JobInfo &info);
+
+
 private:
+    void		GROUP(UT_String &str, fpreal t) { evalString(str, "group", 0, t); }
     void		ATTRIBUTES(UT_String &str, fpreal t) { evalString(str, "attributes", 0, t); }
     bool		COPYP(fpreal t) { return evalInt("copyp", 0, t); }
 
-};
 
-// Simple mapping to a boost::unordered_map that is meant to be a mapping
-// between point id values and their corresponding point offset.
-typedef boost::unordered_map<exint, GA_Offset> IdOffsetMap;
+    /// This variable is used together with the call to the "checkInputChanged"
+    /// routine to notify the handles (if any) if the input has changed.
+    GU_DetailGroupPair   myDetailGroupPair;
+
+    /// This is the group of geometry to be manipulated by this SOP and cooked
+    /// by the method "cookInputGroups".
+    const GA_PointGroup *myGroup;
+
+};
 
 #endif
