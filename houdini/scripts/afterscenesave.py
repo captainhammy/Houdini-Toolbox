48285c014ac67a3787a0340a4f0e1698d43695c9
Graham Thompson <captainhammy@gmail.com>
2017-04-12 06:26:40 -0700

    Add event scripts for before/after scene save.  These scripts will attempt to run any registered events.

---
+++ houdini/scripts/afterscenesave.py
@@ -0,0 +1,5 @@
+from ht.events import runEvents
+
+# Perform any registered after scene save events.
+runEvents("AfterSceneSave", kwargs)
+
---
+++ houdini/scripts/beforescenesave.py
@@ -0,0 +1,5 @@
+from ht.events import runEvents
+
+# Perform any registered before scene save events.
+runEvents("BeforeSceneSave", kwargs)
+
