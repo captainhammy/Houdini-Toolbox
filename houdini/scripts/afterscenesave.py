56a5713fea0ef81b8761805d008f3f069fad2765
Graham Thompson <captainhammy@gmail.com>
2019-02-17 15:25:13 -0800

    Fix headers.

--- houdini/scripts/afterscenesave.py
+++ houdini/scripts/afterscenesave.py
@@ -1,5 +1,13 @@
+"""Perform any registered after scene save events."""
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+# Houdini Toolbox Imports
 from ht.events import SceneEvents, run_event
 
-# Perform any registered after scene save events.
+# =============================================================================
+
 run_event(SceneEvents.PostSave, kwargs)
 
--- houdini/scripts/beforescenesave.py
+++ houdini/scripts/beforescenesave.py
@@ -1,5 +1,13 @@
+"""Perform any registered before scene save events."""
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+# Houdini Toolbox Imports
 from ht.events import SceneEvents, run_event
 
-# Perform any registered before scene save events.
+# =============================================================================
+
 run_event(SceneEvents.PreSave, kwargs)
 
