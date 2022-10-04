1125f9bddd63746fc76f0ef5039cda1612b6f3a1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 05:27:26 +0000

    Adding numeric_owner as keyword arguement

--- python/houdini_toolbox/machinery/package.py
+++ python/houdini_toolbox/machinery/package.py
@@ -700,7 +700,7 @@ class HoudiniInstallFile(HoudiniBase):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(handle, temp_path)
